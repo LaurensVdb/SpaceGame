d74ac848078f2d8dc5fa565faf3d54afa5bac48e
LaurensVdb <13851863+LaurensVdb@users.noreply.github.com.>
2024-08-20 23:33:12 +0200

    Fixes #6:Player hitpoints decrease when hitting an enemy

--- behavior/GameWorld.cs
+++ behavior/GameWorld.cs
@@ -7,133 +7,174 @@ using Raylib_cs;
 
 namespace Bevahior;
 
-public sealed class GameWorld{
-    public IGameObjectRepository GameObjectRepository{ get; set; }
-    public IGameCamera GameCamera{ get; set; }
-    public IEnemySpawner EnemySpawner{ get; set; }
+public sealed class GameWorld
+{
+    public IGameObjectRepository GameObjectRepository { get; set; }
+    public IGameCamera GameCamera { get; set; }
+    public IEnemySpawner EnemySpawner { get; set; }
 
     public IParticleSpawner ParticleSpawner { get; set; }
 
     private int screenWidth;
     private int screenHeight;
-    private int wave=1;
-    public GameWorld(IGameObjectRepository gameObjectRepository,IGameCamera camera,IEnemySpawner enemySpawner,IParticleSpawner particleSpawner,int screenwidth, int screenheight)
+    private int wave = 1;
+    public GameWorld(IGameObjectRepository gameObjectRepository, IGameCamera camera, IEnemySpawner enemySpawner, IParticleSpawner particleSpawner, int screenwidth, int screenheight)
     {
-        this.GameObjectRepository = gameObjectRepository; 
-        this.EnemySpawner = enemySpawner; 
+        this.GameObjectRepository = gameObjectRepository;
+        this.EnemySpawner = enemySpawner;
         this.GameCamera = camera;
-        this.screenHeight = screenheight; 
+        this.screenHeight = screenheight;
         this.screenWidth = screenwidth;
         this.ParticleSpawner = particleSpawner;
         CreateCameraForPlayer();
-       
+
     }
-    
-    private void CreateCameraForPlayer(){
-        GameCamera.CreateCamera(GameObjectRepository.Player,screenWidth , screenHeight);
+
+    private void CreateCameraForPlayer()
+    {
+        GameCamera.CreateCamera(GameObjectRepository.Player, screenWidth, screenHeight);
     }
-    private void StickCameraToplayer(){
+    private void StickCameraToplayer()
+    {
         GameCamera.TargetObject(GameObjectRepository.Player);
     }
 
-    private void SpawnEnemies(){
-        EnemySpawner.StartEnemySpawner(screenWidth,screenHeight,GameObjectRepository,wave);
+    private void SpawnEnemies()
+    {
+        EnemySpawner.StartEnemySpawner(screenWidth, screenHeight, GameObjectRepository, wave);
     }
-      private void SpawnParticles(){
-        ParticleSpawner.StartParticleSpawner(screenWidth,screenHeight,GameObjectRepository);
+    private void SpawnParticles()
+    {
+        ParticleSpawner.StartParticleSpawner(screenWidth, screenHeight, GameObjectRepository);
     }
-    public void CreatePlayer(BaseGameEntity player){
+    public void CreatePlayer(BaseGameEntity player)
+    {
         GameObjectRepository.SetPlayer(player);
         CreateCameraForPlayer();
     }
 
-    public void DrawWorld(){
+    public void DrawWorld()
+    {
         GameCamera.SetCamera();
-        foreach(var entities in GameObjectRepository.Entities){
+        foreach (var entities in GameObjectRepository.Entities)
+        {
             entities.Draw();
         }
         GameObjectRepository.Player.Draw();
-        Raylib.EndMode2D(); 
+        Raylib.EndMode2D();
         GameObjectRepository.Player.DrawInfo();
-         Raylib.DrawText($"Wave {wave}",20, 80, 20, Color.Gold);
+        Raylib.DrawText($"Wave {wave}", 20, 80, 20, Color.Gold);
         /*foreach(var entities in GameObjectRepository.Entities){
             if(entities is Star)entities.Draw();
         }*/
     }
-    public void UpdateWorld(){
+    public void UpdateWorld()
+    {
         SpawnEnemies();
         SpawnParticles();
         CollisionCheck();
         CheckWave();
 
         var gameEntities = GameObjectRepository.Entities.ToList();
-        foreach(var entity in gameEntities){
-          
-            if(entity is Enemy) {
-                entity.Move((int)GameObjectRepository.Player.X,(int)GameObjectRepository.Player.Y);
-                if(entity.CanShoot){
+        foreach (var entity in gameEntities)
+        {
+
+            if (entity is Enemy)
+            {
+                entity.Move((int)GameObjectRepository.Player.X, (int)GameObjectRepository.Player.Y);
+                if (entity.CanShoot)
+                {
                     entity.Shoot(GameObjectRepository);
                 }
 
-            }else{
+            }
+            else
+            {
                 entity.Move();
             }
-            
+
         }
-        
+
         //var newpos = Raylib.GetWorldToScreen2D(new Vector2(GameObjectRepository.Player.X,GameObjectRepository.Player.Y),GameCamera.GetCamera2D()));
-        GameObjectRepository.Player.Move((int)GameCamera.GetCamera2D().Offset.X,(int)GameCamera.GetCamera2D().Offset.Y);
+        GameObjectRepository.Player.Move((int)GameCamera.GetCamera2D().Offset.X, (int)GameCamera.GetCamera2D().Offset.Y);
         GameObjectRepository.Player.Shoot(GameObjectRepository);
         StickCameraToplayer();
     }
 
 
-    private void CheckWave(){
-        if(GameObjectRepository.Player.KillCount>=(10*wave)){
-            wave+=1;
+    private void CheckWave()
+    {
+        if (GameObjectRepository.Player.KillCount >= (10 * wave))
+        {
+            wave += 1;
         }
     }
-    private void CollisionCheck(){
+    private void CollisionCheck()
+    {
 
-        
-        var bullets = GameObjectRepository.Entities.Where(p=>p is Bullet) .ToList();
-        var enemies = GameObjectRepository.Entities.Where(p=>p is Enemy).ToList();
-         foreach(Bullet bullet in bullets ){
-             foreach(Enemy enemy in enemies){
-                if(!bullet.IsEnemy){
+        var bullets = GameObjectRepository.Entities.Where(p => p is Bullet).ToList();
+        var enemies = GameObjectRepository.Entities.Where(p => p is Enemy).ToList();
+
+        foreach (Bullet bullet in bullets)
+        {
+            foreach (Enemy enemy in enemies)
+            {
+                //check if player bullet hits enemy    
+                if (!bullet.IsEnemy)
+                {
                     var iscollsion = bullet.IsCollision(enemy);
-                    if(iscollsion) {
+                    if (iscollsion)
+                    {
                         GameObjectRepository.Entities.Remove(bullet);
-                        if(!enemy.IsAlive){
+                        if (!enemy.IsAlive)
+                        {
                             GameObjectRepository.Entities.Remove(enemy);
-                            GameObjectRepository.Player.KillCount+=1;
+                            //increase killcount. this affects the wave system!
+                            GameObjectRepository.Player.KillCount += 1;
 
                         }
                     }
-                }else{
-                     var iscollsion = bullet.IsCollision( GameObjectRepository.Player);
-                      if(iscollsion) {
+                }
+                else
+                {
+                    //enemy bullet hits player
+                    var iscollsion = bullet.IsCollision(GameObjectRepository.Player);
+                    if (iscollsion)
+                    {
                         GameObjectRepository.Entities.Remove(bullet);
-                        if(!enemy.IsAlive){
+                        if (!enemy.IsAlive)
+                        {
                             GameObjectRepository.Entities.Remove(enemy);
-                        
+                            if (!enemy.IsAlive)
+                            {
+                                GameObjectRepository.Entities.Remove(enemy);
+                                //increase killcount. this affects the wave system!
+                                GameObjectRepository.Player.KillCount += 1;
+
+                            }
 
                         }
                     }
                 }
             }
-         }
-      
+           
 
+        }
 
-        foreach(var entity in enemies){
-            var checkObjects = enemies.Where(p=>p is Enemy).ToList();
-          
+        foreach (var entity in enemies)
+        {
+            var checkObjects = enemies.Where(p => p is Enemy).ToList();
+            //we don't exclude the enemy.
             checkObjects.Remove(entity);
-            foreach(var checkEntity in checkObjects){
-                  var result = entity.IsCollision(checkEntity);
+
+            //check if enemy is hitting an other an enemy
+            foreach (var checkEntity in checkObjects)
+            {
+                entity.IsCollision(checkEntity);
             }
-         
+
+            entity.IsCollision(GameObjectRepository.Player);
+
         }
     }
 
--- game-objects/Enemy.cs
+++ game-objects/Enemy.cs
@@ -8,92 +8,118 @@ namespace GameObjects;
 public class Enemy : BaseGameEntity
 {
 
-
-    private bool shoot;
     private Stopwatch timer;
 
-    protected override Rectangle CollisionRectangle => new Rectangle(X,Y,Widht,Height);
+    protected override Rectangle CollisionRectangle => new Rectangle(X, Y, Widht, Height);
 
-    public Enemy(float x, float y,float movementSpeed,int hitPoints,Texture2D texture2D,bool canShoot=false) : base(x,y,movementSpeed,hitPoints,texture2D,canShoot)
+    public Enemy(float x, float y, float movementSpeed, int hitPoints, Texture2D texture2D, bool canShoot = false) : base(x, y, movementSpeed, hitPoints, texture2D, canShoot)
     {
-    
+
         timer = new Stopwatch();
         timer.Start();
     }
     public override void Draw()
     {
-     
+
         //Raylib.DrawCircleGradient((int)X, (int)Y,Widht, Color.Black, Color.DarkBlue);
-        if(IsAlive){
-            int newWidth = Widht  / HitPointsAtStart;
-            Raylib.DrawRectangle((int)X,(int)Y -10 ,newWidth*HitPoints,5,Color.Gold);
-            Raylib.DrawTextureV(Texture,new Vector2(X,Y),Color.White);
-            
+        if (IsAlive)
+        {
+            int newWidth = Widht / HitPointsAtStart;
+            Raylib.DrawRectangle((int)X, (int)Y - 10, newWidth * HitPoints, 5, Color.Gold);
+            Raylib.DrawTextureV(Texture, new Vector2(X, Y), Color.White);
+
         }
-       
+
         //Raylib.DrawRectangle((int)CollisionRectangle.X-Widht,(int)CollisionRectangle.Y-Widht,(int)CollisionRectangle.Width*2,(int)CollisionRectangle.Height*2,Color.Red);
     }
 
-   
+
     public override void Move(int targetX, int targetY)
     {
-        if(IsMoving){
-            if(X <= targetX){
-                X +=MovementSpeed;
+        if (IsMoving)
+        {
+            if (X <= targetX)
+            {
+                X += MovementSpeed;
             }
-            if(X >= targetX){
-                X -=MovementSpeed;
+            if (X >= targetX)
+            {
+                X -= MovementSpeed;
             }
-            if(Y <= targetY){
-                Y +=MovementSpeed;
+            if (Y <= targetY)
+            {
+                Y += MovementSpeed;
             }
-            if(Y >= targetY){
-                Y -=MovementSpeed;
+            if (Y >= targetY)
+            {
+                Y -= MovementSpeed;
             }
         }
     }
 
 
-    public override void Shoot(IGameObjectRepository gameObjectRepository){
-        var deltaX = gameObjectRepository.Player.X -X; 
-        var deltaY = gameObjectRepository.Player.Y -Y; 
-        var playerrotation =MathF.Atan2(deltaY, deltaX) * (180f / MathF.PI) +90;
-        if(timer.ElapsedMilliseconds>=2000) {
+    public override void Shoot(IGameObjectRepository gameObjectRepository)
+    {
+        var deltaX = gameObjectRepository.Player.X - X;
+        var deltaY = gameObjectRepository.Player.Y - Y;
+        var playerrotation = MathF.Atan2(deltaY, deltaX) * (180f / MathF.PI) + 90;
+        if (timer.ElapsedMilliseconds >= 2000)
+        {
             timer.Reset();
-                    gameObjectRepository.AddEntity(new Bullet(X,Y,5,5,10f,0,playerrotation,true));
-            timer.Start();   
+            gameObjectRepository.AddEntity(new Bullet(X, Y, 5, 5, 10f, 0, playerrotation, true));
+            timer.Start();
         }
-     
+
     }
 
     public override bool IsCollision(BaseGameEntity entityCollisionCheck)
-    {   
-        var isCollision= base.IsCollision(entityCollisionCheck);
-        if(entityCollisionCheck is Enemy){
-            if(isCollision){
-                if(X <= entityCollisionCheck.X){
-                    X-=MovementSpeed;
-                    entityCollisionCheck.X+=MovementSpeed;
+    {
+        var isCollision = base.IsCollision(entityCollisionCheck);
+        switch (entityCollisionCheck)
+        {
+
+
+            case Enemy i:
+                {
+                    if (isCollision)
+                    {
+                        if (X <= entityCollisionCheck.X)
+                        {
+                            X -= MovementSpeed;
+                            entityCollisionCheck.X += MovementSpeed;
+                        }
+                        if (X >= entityCollisionCheck.X)
+                        {
+                            X += MovementSpeed;
+                            entityCollisionCheck.X -= MovementSpeed;
+                        }
+                        if (Y <= entityCollisionCheck.Y)
+                        {
+                            Y -= MovementSpeed;
+                            entityCollisionCheck.Y += MovementSpeed;
+                        }
+                        if (Y >= entityCollisionCheck.Y)
+                        {
+                            Y += MovementSpeed;
+                            entityCollisionCheck.Y -= MovementSpeed;
+                        }
+                    }
+                    break;
                 }
-                if(X>=entityCollisionCheck.X){
-                    X+=MovementSpeed;
-                    entityCollisionCheck.X-=MovementSpeed;
+            case Player i:
+                {
+                    if (isCollision && IsAlive)
+                    {
+                        i.TakeDamage(1);
+                        IsAlive = false;
+                    }
+                    break;
                 }
-                if(Y <= entityCollisionCheck.Y){
-                   Y-=MovementSpeed;
-                   entityCollisionCheck.Y+=MovementSpeed;
-                }
-                if(Y>=entityCollisionCheck.Y){
-                    Y+=MovementSpeed;
-                    entityCollisionCheck.Y-=MovementSpeed;
-                }
-            }
-          
         }
 
         return isCollision;
     }
-    
+
 
 
 }
